02683a755c6dad0aadfb00dfe6d1cfb02abb6267
MMDavito <davidking9753@gmail.com>
2024-03-07 16:43:12 +0100

    If I keep my finger on the SPI lines AND the RCK_OUTPUT, The counter works.
    I will test increase clock frequency, then I will test 51 ohm resistors.
    If still fail, I will go to 220 ohm
    Then setup my osciliscope.

--- 2024-03-07_8_switch_rgb_pcb_rev1/2024-03-07_rev_1_proof_of_function/2024-03-07_rev_1_proof_of_function.ino
+++ 2024-03-07_8_switch_rgb_pcb_rev1/2024-03-07_rev_1_proof_of_function/2024-03-07_rev_1_proof_of_function.ino
@@ -1,17 +1,109 @@
-#define MOSI 11
-#define MISO 12 //74hc165
-#define SCK 13
+#include <SPI.h>
+/*
+  #define MOSI 11
+  #define MISO 12 //74hc165
+  #define SCK 13
+*/
 
-#define OUTPUT_ENABLE
-#define RCK_OUTPUT
-#define RCK_INPUT
+#define BUTTON 3 //5
+#define OUTPUT_ENABLE 2 //4
+#define RCK_INPUT 7 //13
+#define RCK_OUTPUT 8 //14
+
+const byte outputs[6]  =
+{
+  0b10000000,
+  0b01000000,
+  0b00100000,
+  0b00010000,
+  0b00001000,
+  //0b00000100,
+  0x0f,
+};
+
+volatile byte outputValues[6] = {
+  0b11000000,
+  0b01000000,
+  0b00100000,
+  0b00010000,
+  0b00001000,
+  0b00000101,
+};
+
+volatile int counter = 0x0f;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  SPI.begin();
 
+  //SPI.setDataMode(SPI_MODE0); // default - don't need
+  //SPI.setDataMode(SPI_MODE2); //https://docs.arduino.cc/learn/communication/spi/
+
+  SPI.setBitOrder(LSBFIRST); // MSBFIRST is default - leave out in that case
+
+
+  SPI.setClockDivider(SPI_CLOCK_DIV128); // why so slow?  might as well use shiftout()
+  // HC595 and TPIC6B595 work just fine at default speed (4 MHz)
+
+  pinMode(BUTTON, INPUT);
+  pinMode(OUTPUT_ENABLE, OUTPUT);
+  pinMode(RCK_INPUT, OUTPUT);
+  pinMode(RCK_OUTPUT, OUTPUT);
+
+  digitalWrite(OUTPUT_ENABLE, HIGH);
+  //digitalWrite(RCK_INPUT, HIGH);
+  digitalWrite(RCK_INPUT, LOW);
+  digitalWrite(RCK_OUTPUT, HIGH);
+}
+
+
+void printByte(byte val) {
+  for (int i = 7; i >= 0; i--)
+  {
+    bool b = bitRead(val, i);
+    Serial.print(b);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Need to write 16bitWord
+  if (counter == 255)
+    counter = 0;
+  else
+    counter ++;
+  
+Serial.print("Counter: ");
+printByte(counter);
+Serial.println('\n');
 
+
+  long start = millis();
+  while (millis() - start < 500) {
+  //while (millis() - start < 5000) {
+    for (int i = 5; i < 6; i ++) {
+      unsigned long combined = 0; // clear it out
+
+      //If switch to MSB, switch order here.
+      if (i == 5)
+        //combined = (counter << 8) | (outputs[i]);
+        combined = (outputs[i] << 8) | (counter);
+      else
+        combined = (outputValues[i] << 8) | (outputs[i]);
+
+      digitalWrite(RCK_OUTPUT, LOW);
+      delay(50);
+
+      //digitalWrite(RCK_OUTPUT, LOW);
+      //SPI.transfer(combined);
+      //SPI.transfer(outputs[i]);
+      //SPI.transfer(counter);
+      SPI.transfer(0x0F);
+      //SPI.transfer(0XF1);
+      SPI.transfer(counter);
+
+      digitalWrite(OUTPUT_ENABLE, HIGH);
+      digitalWrite(RCK_OUTPUT, HIGH);
+      digitalWrite(OUTPUT_ENABLE, LOW);
+    }
+  }
 }
