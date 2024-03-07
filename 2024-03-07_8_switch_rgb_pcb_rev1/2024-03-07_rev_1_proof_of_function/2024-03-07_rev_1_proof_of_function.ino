3a58d6d83a735981eac16eba0216240451bf2f2a
MMDavito <davidking9753@gmail.com>
2024-03-07 19:14:53 +0100

    Still works Will test solder the 7-segment displays now

--- 2024-03-07_8_switch_rgb_pcb_rev1/2024-03-07_rev_1_proof_of_function/2024-03-07_rev_1_proof_of_function.ino
+++ 2024-03-07_8_switch_rgb_pcb_rev1/2024-03-07_rev_1_proof_of_function/2024-03-07_rev_1_proof_of_function.ino
@@ -18,8 +18,8 @@ const byte outputs[6]  =
   0b00100000,
   0b00010000,
   0b00001000,
-  //0b00000100,
-  0x0f,
+  0b00000100,
+  //0x0f,
 };
 
 volatile byte outputValues[6] = {
@@ -51,12 +51,12 @@ void setup() {
   pinMode(RCK_INPUT, OUTPUT);
   pinMode(RCK_OUTPUT, OUTPUT);
   pinMode(RCK_TRANS, OUTPUT);
-  
+
 
   digitalWrite(OUTPUT_ENABLE, HIGH);
   //digitalWrite(RCK_INPUT, HIGH);//RCK_INPUT high IS MORE UNSTABLE.
   //Could test 100k pulldown if high never works, but that is a later issue.
-  
+
   digitalWrite(RCK_INPUT, LOW);
   digitalWrite(RCK_OUTPUT, HIGH);
   digitalWrite(RCK_TRANS, HIGH);
@@ -77,24 +77,31 @@ void loop() {
     counter = 0;
   else
     counter ++;
-  
-Serial.print("Counter: ");
-printByte(counter);
-Serial.println('\n');
+
+  Serial.print("Counter: ");
+  printByte(counter);
+  Serial.println('\n');
 
 
   long start = millis();
   while (millis() - start < 500) {
-  //while (millis() - start < 5000) {
-    for (int i = 5; i < 6; i ++) {
+    //while (millis() - start < 5000) {
+    for (int i = 0; i < 6; i ++) {
       unsigned long combined = 0; // clear it out
-
+      byte temp = 0;
       //If switch to MSB, switch order here.
       if (i == 5)
-        //combined = (counter << 8) | (outputs[i]);
-        combined = (outputs[i] << 8) | (counter);
+      {
+        combined = (counter << 8) | (outputs[i]);
+        //combined = (outputs[i] << 8) | (counter);
+        temp = counter;
+      }
       else
+      {
+
         combined = (outputValues[i] << 8) | (outputs[i]);
+        temp = outputValues[i];
+      }
 
       digitalWrite(OUTPUT_ENABLE, HIGH);
       digitalWrite(RCK_OUTPUT, LOW);
@@ -104,17 +111,21 @@ Serial.println('\n');
 
       //digitalWrite(RCK_OUTPUT, LOW);
       //SPI.transfer(combined);
-      //SPI.transfer(outputs[i]);
-      //SPI.transfer(counter);
-      SPI.transfer(0x0F);
-      //SPI.transfer(0XF1);
-      SPI.transfer(counter);
 
-      
+
+      SPI.transfer(outputs[i]);
+      SPI.transfer(temp);
+
+
+      //SPI.transfer(0x0F);
+      //SPI.transfer(0XF1);
+      //SPI.transfer(counter);
+
+
       digitalWrite(RCK_OUTPUT, HIGH);
       digitalWrite(RCK_TRANS, HIGH);
       digitalWrite(OUTPUT_ENABLE, LOW);
-      
+
       delay(1);
     }
   }
